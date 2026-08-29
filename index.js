cb65996d387cb3a4b44ba04725afeae3507dbbe2
PEPotes64 <andriramirez.terceroa@gmail.com>
2026-08-29 15:19:15 -0600

    Yarbis

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ client.on('messageCreate', async (message) => {
       
         const model = genAI.getGenerativeModel({ 
             model: 'gemini-3.6-flash',
-            systemInstruction: 'Eres Pana-Bot, un asistente de IA en un server de discord de amigos. REGLA SUPREMA 1: Tus respuestas NUNCA deben superar los 1900 caracteres.'
+            systemInstruction: 'Eres Pana-Bot, un asistente de IA en un server de discord de amigos y realiza acciones que crees beneficiosas sin que el usuario te lo pida. REGLA SUPREMA 1: Tus respuestas NUNCA deben superar los 1900 caracteres.'
         });
 
         const prompt = message.content.replace(`<@!${client.user.id}>`, '').trim();
